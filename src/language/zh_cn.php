35ddf464919f4fcb0d4174d19e84a0baaf4ceba0
范明明 <mingming@live.com>
2023-12-08 22:19:37 +0800

    Added Simplified Chinese.
    Added Simplified Chinese language translation.

---
+++ src/language/zh_cn.php
@@ -0,0 +1,130 @@
+<?php
+
+$translation =  [
+
+    'header' => [
+
+        'home' => '首页',
+        'films' => '电影',
+        'series' => '电视剧',
+        'search' => '搜索',
+        'login' => '登陆',
+        'view_all' => '查看全部'
+    ],
+
+    'index' => [
+
+        'home__title_no_logged' => '推荐剧集',
+        'home__title_recents' => '最近观看',
+        'news' => '新闻',
+        'mobile_options' => '设置',
+        'suggestions' => '建议'
+    ],
+
+    'login' => [
+
+        'username' => '用户名',
+        'password' => '密码',
+        'login' => '登陆',
+        'not_account' => "没有账户?",
+        'sign_up' => '注册',
+        'sign_in' => '登陆'
+
+    ],
+
+    'register' => [
+
+        'name' => '名字',
+        'email' => 'Email',
+        'username' => '用户名',
+        'password' => '密码',
+        'sign_up' => '注册',
+        'already_account' => '已有账户?',
+        'sign_in' => '登陆'
+    ], 
+
+    'details' => [
+
+        'director' => '导演',
+        'cast' => '主演',
+        'genre' => '类型',
+        'release' => '发布时间',
+        'time_min' => '时长',
+        'country' => '国家',
+
+        'watch' => '观看',
+        'add_list' => '添加到列表',
+        'remove_list' => '从列表删除',
+
+        'season' => '季',
+        'seasons' => '季',
+        'episode' => '集'
+
+
+    ],
+
+    'catalog' => [
+
+        'catalog' => '目录',
+        'category' => '种类',
+        'genre' => '类别',
+        'series' => '系列',
+        'films' => '电影'
+    ],
+
+    'search' => [
+        'found' => '找到结果',
+        'title_not_found' => '哎呀,什么都没找到 :/',
+        'search' => '搜索'
+    ],
+
+    'profile' => [
+        'home' => '我的首页',
+        'list' => '我的列表',
+        'watched' => '最近观看',
+        'settings' => '设置',
+        'later' => '稍后再看',
+        'later_no' => "您尚未将任何内容保存到列表中",
+        'recent' => '最近观看的30个节目',
+        'recent_no' => '看点什么怎么样?',
+
+        'title_profile_details' => '详细配置',
+
+        'title_new_password' => '设置密码',
+        'new_password' => '新密码',
+        'confirm_password' => '确认密码',
+
+        'title_xtream' => '更新Xtream列表',
+        'url' => 'URL',
+
+        'save' => '保存',
+        'update' => '更新'
+    ],
+
+
+    'err404' => [
+        'description' => '您访问的页面不可用!',
+        'btn' => '返回'
+    ],
+
+    'warnings' => [
+
+
+        'name_invalid' => '名称最多可以包含60个字符。',
+        'email_invalid' => '电子邮件无效，请输入有效的电子邮件。',
+        'username_invalid' => '用户名包含非法字符或长度超过30个字符。',
+        'pass_invalid' => '密码最多可包含50个字符。',
+        'user_already_exists' => '用户已存在，请创建一个新用户。',
+        'pass_user_invalid' => '用户名或密码不正确。',
+        'generic' => '出现错误，请重试。',
+        'create_account' => '用户注册成功。',
+        'caracteres_search' => '您需要提供最少2个字符或最多20个字符进行搜索。',
+        'not_logged' => '用户未登录。',
+        'edit_success' => '配置文件已成功保存！',
+        'edit_error' => '更新个人资料时出错，请重试。',
+        'new_pass_error' => '为输入相同的密码或字符输入错误。',
+        'xtream_success' => '您的列表已成功更新！',
+        'xtream_error' => '更新列表时出错，请检查您输入是否正确。'
+        ]
+
+];
